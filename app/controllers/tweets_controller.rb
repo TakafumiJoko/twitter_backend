b56a5033261c584125d8e9aa7b50aa47f6de5b64
joko-takafumi <jktkfm5535@gmail.com>
2023-03-11 21:07:14 +0900

    Destroy Tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,20 @@
 class TweetsController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: [:create]
+  before_action :set_tweet, only: [:destroy]
 
   def create
     tweet = @user.tweets.create(tweet_params)
     render json: {
       status: :created,
-      tweet: tweet
+      tweet: tweet,
+    }
+  end
+
+  def destroy
+    @tweet.destroy
+    tweet = Tweet.find_by(id: params[:id])
+    render json: {
+      status: :destroyed,
     }
   end
 
@@ -15,6 +24,10 @@ class TweetsController < ApplicationController
     end
 
     def set_user
-      @user = User.find(params[:user_id])
+      @user = User.find_by(id: params[:user_id])
+    end
+
+    def set_tweet
+      @tweet = Tweet.find_by(id: params[:id])
     end
 end
