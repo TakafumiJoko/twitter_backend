1ff7cfbe060ea01866e26fee1c95884a86cc942f
joko-takafumi <jktkfm5535@gmail.com>
2023-03-16 20:53:55 +0900

    Search Tweet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::API
   def search
     users = User.where(nickname: params[:key]).or(User.where(name: params[:key]))
+    searched_tweets = Tweet.where("message LIKE ?", "%#{params[:key]}%")
     render json: {
       status: :searched,
       users: users,
+      tweets: searched_tweets,
     }
   end
 end
